b37f454212c5d2a8e58173fcf12dd231744f1a11
zwx520211314 <51103767+zwx520211314@users.noreply.github.com>
2019-05-28 10:47:50 +0800

    Add files via upload

---
+++ WeatherData.py
@@ -0,0 +1,29 @@
+
+import requests as req
+import json
+class WeatherDate:
+    temperature = ''
+    humid = ''
+    key = 'bf1b626f3ed6c480088d28d9a053e358'
+    url = 'http://api.openweathermap.org/data/2.5/weather'
+    icon = ''
+    def __init__(self, city):
+        self.city = city
+        apiURL = '{url}?q={city}&APPID={key}'.format(
+        url = self.url,
+        city = self.city,
+        key = self.key
+        )
+        print(apiURL)
+        r = req.get(apiURL)
+        obj = json.loads(r.text)
+        self.icon = obj['weather'][0]['icon']
+        self.humid = obj['main']['humidity']
+    def getTemperature(self):
+         print(type(self.temperature))
+         return str(self.temperature)
+    def getHumid(self):
+        return self.humid 
+if __name__ == "__main__":
+    current_weather = WeatherDate('Shanghai, cn')
+    print(current_weather.getHumid())
